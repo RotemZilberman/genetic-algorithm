11e1455f9e75d7d79a621d2fbfe1a6d271a05fb7
ohadlanger <118103585+ohadlanger@users.noreply.github.com>
2024-02-28 17:49:52 +0200

    Abstract class + QueensProblem

--- problem1/bin.py
+++ problem1/bin.py
@@ -1,10 +1,11 @@
 import argparse
+from gen_algorithm import EightQueenPuzzle
 
 Population = 100
-Generations = 100
+Generations = 1000
 Patience = 10
 MutationRate = 0.1
-CrossoverRate = 0.9
+CrossoverRate = 0.1
 EliteSize = 2
 
 
@@ -13,6 +14,8 @@ def main():
         Population, Generations, Patience, MutationRate, CrossoverRate, EliteSize)
     gen_alg = EightQueenPuzzle(population, generations, patience, mutation_rate, crossover_rate, elite_size)
     best_list, avg_eval_list, best_eval = gen_alg.run()
+    print(f'{best_list[0]} to {best_list[-1]}')
+    # print(gen_alg.fitness_function([0, 4, 7, 5, 2, 6, 1, 3]))
 
 
 if __name__ == '__main__':
--- problem1/gen_algorithm.py
+++ problem1/gen_algorithm.py
@@ -1,10 +1,29 @@
-import random
+import math
 from abc import ABC, abstractmethod
+import random
+
+
+def calculate_x(m):
+    # Calculate the discriminant
+    discriminant = 1 + 8 * m
+
+    # Check if the discriminant is non-negative
+    if discriminant >= 0:
+        # Calculate the positive value of x
+        x = (-1 + discriminant ** 0.5) / 2
+        return math.floor(x)
+    else:
+        return 0
 
 
 class GeneticAlgorithm(ABC):
     def __init__(self, population, generations, patience, mutation_rate, crossover_rate, elite_size):
-        pass
+        self.population = population
+        self.generations = generations
+        self.patience = patience
+        self.mutation_rate = mutation_rate
+        self.crossover_rate = crossover_rate
+        self.elite_size = elite_size
 
     @abstractmethod
     def generate_population(self):
@@ -14,22 +33,61 @@ class GeneticAlgorithm(ABC):
     def mutate(self, individual):
         pass
 
-    def getElite(self, pop_eval):
-        pass
+    def get_elite(self, pop_eval):
+        top = pop_eval[-1 * self.elite_size:]
+        return [chromo for (chromo, evaluation) in top]
 
     @abstractmethod
     def fitness_function(self, individual):
         pass
 
     def evaluate_all(self, population):
-        pass
+        pop_eval = []
+        for chromo in population:
+            pop_eval.append((chromo, self.fitness_function(chromo)))
 
-    def evolve(self, pop_eval):
-        pass
+        def key(element):
+            return element[1]
+
+        pop_eval.sort(key=key)
+        best_eval = pop_eval[-1][1]
+        avg_eval = 0
+        for element in pop_eval:
+            avg_eval += element[1]
+        avg_eval /= self.population
+        return pop_eval, avg_eval, best_eval
+
+    # def sample_chromos(self, pop_eval):
+    #     rand1 = random.randint(0, self.population-1)
+    #     rand2 = random.randint(0, self.population-1)
+    #     first = pop_eval[rand1][0]
+    #     second = pop_eval[rand2][0]
+    #     return first, second
+
+    def sample_chromos(self, pop_eval):
+        combinations = (self.population * (self.population-1))/2
+        rand1 = random.randint(0, combinations)
+        rand2 = random.randint(0, combinations)
+        first = pop_eval[calculate_x(rand1)][0]
+        second = pop_eval[calculate_x(rand2)][0]
+        return first, second
 
     @abstractmethod
     def crossover(self, parent1, parent2):
         pass
+
+    def evolve(self, pop_eval):
+        population = self.get_elite(pop_eval)
+        count = 0
+        while count < (self.population - self.elite_size):
+            first, second = self.sample_chromos(pop_eval)
+            seed = random.uniform(0, 1)
+            if seed <= self.crossover_rate:
+                child = self.crossover(first, second)
+                population.append(child)
+                count += 1
+
+        return population
 
     def chromosome_to_solution(self, chromosome):
         return chromosome
@@ -53,61 +111,53 @@ class GeneticAlgorithm(ABC):
 
 
 class EightQueenPuzzle(GeneticAlgorithm):
-    def __init__(self, population, generations, mutation, crossover, selection, tournament, fitness, output):
-        super().__init__(population, generations, mutation, crossover, selection, tournament, fitness, output)
+    def __init__(self, population, generations, patience, mutation_rate, crossover_rate, elite_size, cross_mode="single"):
+        super().__init__(population, generations, patience, mutation_rate, crossover_rate, elite_size)
+        self.cross_mode = cross_mode
+        self.board_length = 8
 
     def generate_population(self):
-        pass
+        population = []
+        for i in range(self.population):
+            rand = random.choices(range(0, self.board_length), k=self.board_length)
+            while not self.fitness_function(rand) < 20:
+                rand = random.choices(range(0, self.board_length), k=self.board_length)
+            population.append(rand)
+        return population
 
     # easy to implement - just generate gosian noise between 1-8 for each queen
     def mutate(self, individual):
-        pass
-
-    # count the conflicts between the queens
-    def fitness_function(self, individual):
-        pass
+        seed = random_number = random.uniform(0, 1)
+        if seed < self.mutation_rate:
+            column = random.randint(0, self.board_length-1)
+            index = random.randint(0, self.board_length-1)
+            individual[column] = index
+        return individual
 
     # checking between multiple single points and uniform crossover
     def crossover(self, parent1, parent2):
-        pass
-
-
-class TSP(GeneticAlgorithm):
-    def __init__(self, population, generations, patience, mutation_rate, crossover_rate, elite_size, cities):
-        super().__init__(population, generations, patience, mutation_rate, crossover_rate, elite_size)
-        self.start_city = cities[1]
-        self.cities = cities
-        del cities[1]
-
-    def generate_population(self):
-        pop_size = self.population
-        population = []
-        for _ in range(pop_size):
-            population.append(random.sample(self.cities.keys(), len(self.cities)))
-        return population
-
-    def mutate(self, individual):
-        if random.random() < self.mutation_rate:
-            index1, index2 = random.sample(range(len(individual)), 2)
-            individual[index1], individual[index2] = individual[index2], individual[index1]
-
-    def fitness_function(self, individual):
-        current_distance = 0
-        current_city = self.start_city
-        for city_index in individual:
-            city = self.cities[city_index]
-            current_distance += current_city.distance(city)
-            current_city = city
-        current_distance += current_city.distance(self.start_city)
-        return 1 / current_distance
-
-    def crossover(self, parent1, parent2):
-        single_point = self.population // 2
-        child = parent1[:single_point]
-        for city in parent2:
-            if city not in child:
-                child.append(city)
+        child = []
+        if self.cross_mode == "single":
+            point = random.randint(0, self.board_length-1)
+            child = parent1[:point+1] + parent2[point+1:]
+        elif self.cross_mode == "uniform":
+            for i in range(self.board_length):
+                seed = random.uniform(0, 1)
+                index = parent1[i] if seed <= 0.5 else parent2[i]
+                child.append(index)
+        child = self.mutate(child)
         return child
 
-    def chromosome_to_solution(self, chromosome):
-        return chromosome
+    # count the conflicts between the queens
+    def fitness_function(self, individual):
+        hits = 0
+
+        for col1, row1 in enumerate(individual):
+            for col2, row2 in enumerate(individual):
+                if col1 != col2:
+                    hits += (row1 == row2)  # same row
+                    hits += (((col2-col1) + row1) == row2) or ((row1 - (col2-col1)) == row2)  # same diagonal
+
+        maximum_collisions = (self.board_length * (self.board_length-1))
+        return maximum_collisions - hits
+
